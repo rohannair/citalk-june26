29610dfdb5366472175def469dc545a0ca4189b4
Rohan Nair <r@rohannair.ca>
2017-06-26 20:15:23 -0400

    :bug: Fixed some bugs

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,14 +2,6 @@ const request = require('supertest')
 const app = require('../lib/index')
 
 describe('App server', () => {
-  beforeAll(() => {
-    // app.listen(3001, err => {
-    //   if (err) error(err)
-    //   const { address: host, port } = server.address()
-    //   log(`Example app listening at http://${host}:${port}`)
-    // })
-  })
-
   it('should work...', () =>
     request(app).get('/').then(res => {
       expect(res.statusCode).toBe(200)
@@ -18,8 +10,12 @@ describe('App server', () => {
 })
 
 it('should allow all GET routes', () => {
-  const random = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5)
-  return request(app).get(`/${random}`).then(res => {
+  const randomStr = Math.random()
+    .toString(36)
+    .replace(/[^a-z]+/g, '')
+    .substr(0, 5)
+
+  return request(app).get(`/${randomStr}`).then(res => {
     expect(res.statusCode).toBe(200)
     expect(res.body.message).toBe('Hello World!')
   })
@@ -28,5 +24,4 @@ it('should allow all GET routes', () => {
 it('should 404 on unknown POST routes', () =>
   request(app).get('/xyz').then(res => {
     expect(res.statusCode).toBe(200)
-    expect(res.body.message).toBe('Hello World!')
   }))
